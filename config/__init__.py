2e91c5097c8206a32adc54142009b6d920383e5b
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2025-04-04 17:52:07 +0200

    working on OT

--- config/__init__.py
+++ config/__init__.py
@@ -5,14 +5,16 @@ from pathlib import Path
 
 #### TRAINING CONFIG ####
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-NUM_CONFORMERS = 5
-BATCH_SIZE = 2
+NUM_CONFORMERS = 10
+NUM_CONFORMERS_SAMPLE = 3
+BATCH_SIZE = 1
 NUM_WORKERS = multiprocessing.cpu_count() - 1
 
 #### DATA CONFIG ####
 ROOT = Path(__file__).parent.parent
 TRAIN_DIR = ROOT / "data" / "train"
 TEST_DIR = ROOT / "data" / "test"
+NUM_POSSIBLE_ATOMS = 44
 
 #### GAT CONFIG ####
 EDGE_DIM = 8
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,4 @@
+from models.mpnn.gat import GAT
 from models.mace.mace import MACEModel
+from models.mace_bary.mace_bary import MACEBaryModel
+
