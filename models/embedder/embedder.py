1abe30d2c6f91bcac8881698c5416b0f9e29d305
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2025-04-07 11:18:31 +0200

    embedder ready

---
+++ models/embedder/embedder.py
@@ -0,0 +1,45 @@
+import torch
+
+from torch_geometric.nn import MLP
+
+import config as cfg
+
+
+class FeatureEmbedder(torch.nn.Module):
+    """
+    FeatureEmbedder class.
+    This class is used to embed the node features and the molecule features. using MLPs.
+    The input features are the atom features and the molecule features.
+    The output features are the embedded features.  
+    """
+    
+    def __init__(
+            self, input_mol_emb_dim=cfg.IN_MOL_DIM, 
+            input_node_emb_dim=cfg.NODE_DIM,
+            out_emb_dim=cfg.OUT_EMB_DIM
+        ):
+        super(FeatureEmbedder, self).__init__()
+        self.mlp_u_emb = MLP(
+            in_channels=input_mol_emb_dim,
+            hidden_channels=128,
+            out_channels=out_emb_dim,
+            num_layers=3,
+            act=torch.nn.SiLU(),
+            norm='LayerNorm'
+            )
+                
+        self.mlp_x_emb = MLP(
+            in_channels=input_node_emb_dim,
+            hidden_channels=128,
+            out_channels=out_emb_dim,
+            num_layers=3,
+            act=torch.nn.SiLU(),
+            norm='LayerNorm'
+            )
+            
+    def forward(self, batch):
+        batch.u = self.mlp_u_emb(batch.u)
+        batch.x = self.mlp_x_emb(batch.x)
+        return batch
+        
+        
